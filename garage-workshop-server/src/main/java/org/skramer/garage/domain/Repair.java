71f68915b7a597441c010757249a3ca7207f66f3
Sebastian Kramer <sebastian.kramer@wp.pl>
2016-09-17 14:53:46 +0200

    added defensive copies to repair class

--- garage-workshop-server/src/main/java/org/skramer/garage/domain/Repair.java
+++ garage-workshop-server/src/main/java/org/skramer/garage/domain/Repair.java
@@ -36,7 +36,7 @@ public class Repair {
   public Repair(List<Employee> assignedEmployees, List<GarageTool> assignedTools, Date expectedFinishDate,
                 BigDecimal estimatedCost, String description, String repairNotes) {
     this.description = description;
-    this.expectedFinishDate = expectedFinishDate;
+    this.expectedFinishDate = new Date(expectedFinishDate.getTime());
     this.estimatedCost = estimatedCost;
     this.repairNotes = repairNotes;
     this.assignedEmployees = assignedEmployees;
@@ -65,11 +65,11 @@ public class Repair {
   }
 
   public Date getExpectedFinishDate() {
-    return expectedFinishDate;
+    return new Date(expectedFinishDate.getTime());
   }
 
   public void setExpectedFinishDate(Date expectedFinishDate) {
-    this.expectedFinishDate = expectedFinishDate;
+    this.expectedFinishDate = new Date(expectedFinishDate.getTime());
   }
 
   public BigDecimal getEstimatedCost() {
